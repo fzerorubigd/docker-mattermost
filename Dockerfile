cdf9906c7b3cfcd1e51939ef4c34849a32bbaaef
fzerorubigd <fzerorubigd@gmail.com>
2015-10-05 22:18:28 +0330

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM ubuntu:trusty
+MAINTAINER fzerorubigd <fzero@rubi.gd> @fzerorubigd
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+		wget \
+        jq \
+	&& rm -rf /var/lib/apt/lists/*
+
+
+RUN groupadd -r mattermost && useradd -r -g mattermost mattermost
+
+WORKDIR /home/mattermost/
+
+RUN wget --no-check-certificate https://github.com/mattermost/platform/releases/download/v1.0.0/mattermost.tar.gz \
+         && tar -xvzf mattermost.tar.gz \
+         && rm mattermost.tar.gz
+
+RUN mkdir -p /mattermost/data
+
+RUN chown -R mattermost:mattermost /mattermost/data
+
+VOLUME /mattermost/data
+
+ADD docker-initscript.sh /sbin/docker-initscript.sh
+RUN chmod 755 /sbin/docker-initscript.sh
+EXPOSE 8086/tcp
+ENTRYPOINT ["/sbin/docker-initscript.sh"]
+CMD ["/mattermost/config.json"]
---
+++ docker-initscript.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DB_TYPE=${DB_TYPE:-postgres}
+DB_HOST=${DB_HOST:-${POSTGRESQL_PORT_5432_TCP_ADDR}}
+DB_PORT=${DB_PORT:-${POSTGRESQL_PORT_5432_TCP_PORT}}
+
+# support for linked official postgres image
+DB_USER=${DB_USER:-${POSTGRESQL_ENV_POSTGRES_USER}}
+DB_PASS=${DB_PASS:-${POSTGRESQL_ENV_POSTGRES_PASSWORD}}
+DB_NAME=${DB_NAME:-${DB_USER}}
+
+POSTGRESQL_HOST=${DB_HOST}
+POSTGRESQL_PORT=${DB_PORT}
+FILE=$1
+if [ ${FILE: -5} == ".json" ]; then
+    if [ ! -f ${FILE} ]; then
+        cp /home/mattermost/mattermost/config/config.json ${FILE}
+    fi
+    # overwrite all the keys 
+    
+    JSON=$(cat $FILE | jq '.SqlSettings.DriverName="postgres"' | jq '.SqlSettings.DataSource="postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable&connect_timeout=10"')
+    ENVLIST=$(env | grep MATTERMUST)
+    ALL_ARR=($ENVLIST)
+    for KEY in ${ALL_ARR[@]};do;
+        JSON_KEY=$(echo $KEY | cut -d= -f1 | tr '[:upper:]' '[:lower:]' |sed 's/mattermust/./g' | sed -r 's/(^|_)([a-z])/\U\2/g')
+        JSON_VALUE=$(echo $KEY | cut -d= -f2)
+        JSON=$(echo $JSON| jq '$JSON_KEY="$JSON_VALUE"')
+    done;
+    
+    echo $JSON > ${FILE}
+    /home/mattermost/mattermost/bin/platform -config=${FILE}
+else
+    exec "$@"
+fi;
